a5bb7aa01298bfb3f0553dd528f1bf275eab27a3
jagit1 <jarlathcoyle2@gmail.com>
2023-01-03 19:29:18 +0000

    Updating content on homepage and links on the navbar on homepage and all search pages

--- Scripts/static/js/HomeSlideshow.js
+++ Scripts/static/js/HomeSlideshow.js
@@ -10,7 +10,7 @@ function showSlides() {
   slideIndex++;
   if (slideIndex > slides.length) {slideIndex = 1}
   slides[slideIndex-1].style.display = "block";
-  setTimeout(showSlides, 5000); // Change image every 2 seconds
+  setTimeout(showSlides, 4000); // Change image every 2 seconds
 }
 
 <!--from here on W3 schools https://www.w3schools.com/howto/howto_js_slideshow.asp-->
